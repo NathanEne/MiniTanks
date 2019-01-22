b9745386bd7c8252f50c3380a5e3eabc5a502373
Rylan Marianchuk <rylan.marianchuk@ucalgary.ca>
2019-01-22 15:34:15 -0600

    Creating Project

---
+++ MiniTanks/desktop/src/com/minitanks/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.minitanks.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.minitanks.game.MiniTanksGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniTanksGame(), config);
+	}
+}
