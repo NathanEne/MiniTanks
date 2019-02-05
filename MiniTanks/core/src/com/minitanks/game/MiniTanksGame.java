2d94743d3fe803043d0e6a431c9885a2ee6fb5a3
Rylan Marianchuk <rylan.marianchuk@ucalgary.ca>
2019-02-05 15:26:38 -0600

    Attempted to create a scene

--- MiniTanks/core/src/com/minitanks/game/MiniTanksGame.java
+++ MiniTanks/core/src/com/minitanks/game/MiniTanksGame.java
@@ -2,32 +2,69 @@ package com.minitanks.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.sun.prism.image.ViewPort;
 
 public class MiniTanksGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public OrthographicCamera cam = new OrthographicCamera();
+	public Model model;
+	public ModelInstance instance;
+	public ModelBatch modelBatch;
+	private Viewport vp = new FitViewport(800, 500, cam);
+
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create(){
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render(){
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance);
+		modelBatch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void resize(int w, int h){
+
+	}
+
+	@Override
+	public void pause(){
+
+	}
+
+	@Override
+	public void resume(){
+
 	}
 }
