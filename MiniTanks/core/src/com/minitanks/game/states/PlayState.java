771a15833b26f4e9c4c9249b0793882b5120faea
NathanE <nathan@enerson.ca>
2019-02-06 12:15:32 -0700

    Made game load and use the playstate earlier created

--- MiniTanks/core/src/com/minitanks/game/states/PlayState.java
+++ MiniTanks/core/src/com/minitanks/game/states/PlayState.java
@@ -1,12 +1,19 @@
 package com.minitanks.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 
 public class PlayState extends State {
 
 
-    protected PlayState(GameStateManager gsm) {
+    public PlayState(GameStateManager gsm) {
         super(gsm);
+        Gdx.gl.glClearColor(0f / 255f, 0f / 255f, 0f / 255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        State.cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        State.cam.update();
 
     }
 
