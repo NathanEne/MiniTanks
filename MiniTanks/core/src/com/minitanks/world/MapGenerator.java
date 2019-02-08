87dfac3c46370c3504730b3a37cd28bbadcdef51
rylan-marianchuk <45247316+rylan-marianchuk@users.noreply.github.com>
2019-02-08 14:59:32 -0700

    Update MapGenerator.java

--- MiniTanks/core/src/com/minitanks/world/MapGenerator.java
+++ MiniTanks/core/src/com/minitanks/world/MapGenerator.java
@@ -1,4 +1,27 @@
 package com.minitanks.world;
 
 public class MapGenerator {
+    /*
+     *   3 Random modes of map generation:
+     *          Asymmetric
+     *          Horizontal/Vertically symmetric
+     *          Oblique symmetry
+     *
+     *   Random shapes templates:
+     *          Right angle
+     *          Single Line
+     *          Hexagon Top
+     *          120 degree 'V'
+     *
+     *   1. Choose 5-10 random points within the map (leaving a gap border).
+     *          Instantiate a variant of one of the shape templates
+     *              What we can randomize:
+     *                  Rotation, length of line segments
+     *          After chosen point, block off this radius for next points
+     *          
+     *   2. Then take end points and make a connection with only those nearby
+     *   3. Randomly destroy a few line segments
+     *   4. Rotate if symmetry was required
+     *
+     */
 }
