bdcd83044572456d8cf03c150ac53a549ae8731c
Rylan Marianchuk <rylan.marianchuk@ucalgary.ca>
2019-02-10 17:01:35 -0600

    Create shape generation class

---
+++ MiniTanks/core/src/com/minitanks/world/ShapeGeneration.java
@@ -0,0 +1,26 @@
+package com.minitanks.world;
+
+public class ShapeGeneration {
+    /*
+     *  Implement static methods to generator the various shapes on the map
+     */
+
+    public static float varianceThres = 0f;
+
+
+    public static float[] rightAngle(float r){
+        return new float[]{};
+    }
+
+    public static float[] cutHexagon(float r){
+        return new float[]{};
+    }
+
+    public static float[] line(float r){
+        return new float[]{};
+    }
+
+    public static float[] open120(float r){
+        return new float[]{};
+    }
+}
